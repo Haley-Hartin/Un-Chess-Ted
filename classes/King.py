f5b068024c7d99f9685e0cfab544edb8411c93c6
Blythe Waltman <blwa4890@colorado.edu>
2020-12-01 19:13:04 -0700

    King can move and capture

--- classes/King.py
+++ classes/King.py
@@ -9,11 +9,78 @@ class King(Piece):
         self.isCaptured = False
         #print("Creating " + self.color + " " + self.type)
 
-    def giveMoveList(self, position, ):
-        print("I move like a King")
+    def getID(self):
+        return self.id
+
+    def giveMoveList(self, board):
+        moveList = []
+
+        row = self.position[0]
+        column = self.position[1]
+        if(row + 1 <= 7):
+            if(column - 1 >= 0):
+                if (board.getPiece(row + 1, column - 1) == None):
+                    moveList.append([row + 1, column -1])
+            if (board.getPiece(row + 1, column) == None):
+                moveList.append([row + 1, column])
+            if (column + 1 <= 7):
+                if (board.getPiece(row + 1, column + 1) == None):
+                    moveList.append([row + 1, column + 1])
+
+        if (row - 1 >= 0):
+            if (column - 1 >= 0):
+                if (board.getPiece(row - 1, column - 1) == None):
+                    moveList.append([row - 1, column -1])
+            if (board.getPiece(row - 1, column) == None):
+                moveList.append([row - 1, column])
+            if (column + 1 <= 7):
+                if (board.getPiece(row - 1, column + 1) == None):
+                    moveList.append([row - 1, column + 1])
+
+        if(column - 1 >= 0):
+            if (board.getPiece(row, column - 1) == None):
+                moveList.append([row, column - 1])
+        if (column + 1 <= 7):
+            if (board.getPiece(row, column + 1) == None):
+                moveList.append([row, column + 1])
+
+        return moveList
+
 
     def giveCaptureList(self,board):
-        print("I capture like a King")
+        captureList = []
+
+        row = self.position[0]
+        column = self.position[1]
+
+        if (row + 1 <= 7):
+            if (column - 1 >= 0):
+                if (board.getPiece(row + 1, column - 1) != None and board.getPiece(row + 1, column - 1).getColor() != self.color):
+                    captureList.append([row + 1, column - 1])
+            if (board.getPiece(row + 1, column) != None and board.getPiece(row + 1, column).getColor() != self.color):
+                captureList.append([row + 1, column])
+            if (column + 1 <= 7):
+                if (board.getPiece(row + 1, column + 1) != None and board.getPiece(row + 1, column + 1).getColor() != self.color):
+                    captureList.append([row + 1, column + 1])
+
+        if (row - 1 >= 0):
+            if (column - 1 >= 0):
+                if (board.getPiece(row - 1, column - 1) != None and board.getPiece(row - 1, column - 1).getColor() != self.color):
+                    captureList.append([row - 1, column - 1])
+            if (board.getPiece(row - 1, column) != None and board.getPiece(row - 1, column).getColor() != self.color):
+                captureList.append([row - 1, column])
+            if (column + 1 <= 7):
+                if (board.getPiece(row - 1, column + 1) != None and board.getPiece(row - 1, column + 1).getColor() != self.color):
+                    captureList.append([row - 1, column + 1])
+
+        if (column - 1 >= 0):
+            if (board.getPiece(row, column - 1) != None and board.getPiece(row, column - 1).getColor() != self.color):
+                captureList.append([row, column - 1])
+        if (column + 1 <= 7):
+            if (board.getPiece(row, column + 1) != None and board.getPiece(row, column + 1).getColor() != self.color):
+                captureList.append([row, column + 1])
+
+        return captureList
     
     def is_captured(self):
         return self.isCaptured

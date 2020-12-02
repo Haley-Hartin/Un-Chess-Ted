96e3571410bbc163590f8c2d098675282be81170
Blythe Waltman <blwa4890@colorado.edu>
2020-12-01 17:02:31 -0700

    Bishops can give move list and capture list

--- classes/Bishop.py
+++ classes/Bishop.py
@@ -9,8 +9,129 @@ class Bishop(Piece):
         self.isCaptured = False
         #print("Creating " + self.color + " " + self.type)
 
-    def giveMoveList(self, position, ):
-        print("I move like a Bishop")
+    def getID(self):
+        return self.id
+
+    def giveMoveList(self, board):
+        moveList = []
+        row = self.position[0] + 1
+        column = self.position[1] + 1
+        blocked = False
+
+        #Check all unblocked spaces diagonal up and right
+        while(row <= 7 and column <=7 and blocked == False):
+            if(board.getPiece(row, column) == None):
+                moveList.append([row, column])
+                row = row + 1
+                column = column + 1
+            else:
+                blocked = True
+
+        row = self.position[0] + 1
+        column = self.position[1] - 1
+        blocked = False
+
+        #Check all unblocked spaces diagonal up and left
+        while (row <= 7 and column >= 0 and blocked == False):
+            if (board.getPiece(row, column) == None):
+                moveList.append([row, column])
+                row = row + 1
+                column = column - 1
+            else:
+                blocked = True
+
+        row = self.position[0] - 1
+        column = self.position[1] - 1
+        blocked = False
+
+        # Check all unblocked spaces diagonal down and left
+        while (row >= 0 and column >= 0 and blocked == False):
+            if (board.getPiece(row, column) == None):
+                moveList.append([row, column])
+                row = row - 1
+                column = column - 1
+            else:
+                blocked = True
+
+        row = self.position[0] - 1
+        column = self.position[1] + 1
+        blocked = False
+
+        # Check all unblocked spaces diagonal down and right
+        while (row >= 0 and column <= 7 and blocked == False):
+            if (board.getPiece(row, column) == None):
+                moveList.append([row, column])
+                row = row - 1
+                column = column + 1
+            else:
+                blocked = True
+
+        return moveList
 
     def giveCaptureList(self,board):
-        print("I capture like a Bishop")
+        captureList = []
+        row = self.position[0] + 1
+        column = self.position[1] + 1
+        blocked = False
+
+        # Check all unblocked spaces diagonal up and right
+        while (row <= 7 and column <= 7 and blocked == False):
+            if (board.getPiece(row, column) != None):
+                if(board.getPiece(row, column).getColor() != self.color):
+                    captureList.append([row, column])
+                    blocked = True
+                else:
+                    blocked = True
+            else:
+                row = row + 1
+                column = column + 1
+
+        row = self.position[0] + 1
+        column = self.position[1] - 1
+        blocked = False
+
+        # Check all unblocked spaces diagonal up and left
+        while (row <= 7 and column >= 0 and blocked == False):
+            if (board.getPiece(row, column) != None):
+                if (board.getPiece(row, column).getColor() != self.color):
+                    captureList.append([row, column])
+                    blocked = True
+                else:
+                    blocked = True
+            else:
+                row = row + 1
+                column = column - 1
+
+        row = self.position[0] - 1
+        column = self.position[1] - 1
+        blocked = False
+
+        # Check all unblocked spaces diagonal down and left
+        while (row >= 0 and column >= 0 and blocked == False):
+            if (board.getPiece(row, column) != None):
+                if (board.getPiece(row, column).getColor() != self.color):
+                    captureList.append([row, column])
+                    blocked = True
+                else:
+                    blocked = True
+            else:
+                row = row - 1
+                column = column - 1
+
+        row = self.position[0] - 1
+        column = self.position[1] + 1
+        blocked = False
+
+        # Check all unblocked spaces diagonal down and right
+        while (row >= 0 and column <= 7 and blocked == False):
+            if (board.getPiece(row, column) != None):
+                if (board.getPiece(row, column).getColor() != self.color):
+                    captureList.append([row, column])
+                    blocked = True
+                else:
+                    blocked = True
+            else:
+                row = row - 1
+                column = column + 1
+
+        return captureList

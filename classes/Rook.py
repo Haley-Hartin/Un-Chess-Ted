4ce72b7e2256b9e24d66a0f65acc39c27f17d0a8
Blythe Waltman <blwa4890@colorado.edu>
2020-12-01 18:33:25 -0700

    Rooks can move and capture

--- classes/Rook.py
+++ classes/Rook.py
@@ -9,8 +9,109 @@ class Rook(Piece):
         self.isCaptured = False
         #print("Creating " + self.color + " " + self.type)
 
-    def giveMoveList(self, position, ):
-        print("I move like a Rook")
+    def getID(self):
+        return self.id
+
+    def giveMoveList(self, board):
+        moveList = []
+
+        row = self.position[0] + 1
+        column = self.position[1]
+        blocked = False
+
+        while(row <= 7 and blocked == False):
+            print("First While Loop")
+            if(board.getPiece(row, column) == None):
+                moveList.append([row, column])
+            else:
+                blocked = True
+            row = row + 1
+
+        row = self.position[0] - 1
+        column = self.position[1]
+        blocked = False
+
+        while(row >= 0 and blocked == False):
+            print("Second While Loop")
+            if (board.getPiece(row, column) == None):
+                moveList.append([row, column])
+            else:
+                blocked = True
+            row = row - 1
+
+        row = self.position[0]
+        column = self.position[1] + 1
+        blocked = False
+
+        while (column <= 7 and blocked == False):
+            print("Third While Loop")
+            if (board.getPiece(row, column) == None):
+                moveList.append([row, column])
+            else:
+                blocked = True
+            column = column + 1
+
+        row = self.position[0]
+        column = self.position[1] - 1
+        blocked = False
+
+        while (column >= 0 and blocked == False):
+            print("Fourth While Loop")
+            if (board.getPiece(row, column) == None):
+                moveList.append([row, column])
+            else:
+                blocked = True
+            column = column - 1
+
+        return moveList
+
 
     def giveCaptureList(self,board):
-        print("I capture like a Rook")
+        print("Getting Rook Move List")
+        captureList = []
+
+        row = self.position[0] + 1
+        column = self.position[1]
+        blocked = False
+
+        while (row <= 7 and blocked == False):
+            print("First While Loop")
+            if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
+                captureList.append([row, column])
+                blocked = True
+            row = row + 1
+
+        row = self.position[0] - 1
+        column = self.position[1]
+        blocked = False
+
+        while (row >= 0 and blocked == False):
+            print("Second While Loop")
+            if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
+                captureList.append([row, column])
+                blocked = True
+            row = row - 1
+
+        row = self.position[0]
+        column = self.position[1] + 1
+        blocked = False
+
+        while (column <= 7 and blocked == False):
+            print("Third While Loop")
+            if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
+                captureList.append([row, column])
+                blocked = True
+            column = column + 1
+
+        row = self.position[0]
+        column = self.position[1] - 1
+        blocked = False
+
+        while (column >= 0 and blocked == False):
+            print("Fourth While Loop")
+            if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
+                captureList.append([row, column])
+                blocked = True
+            column = column - 1
+
+        return captureList

3d72862847e3d68dd6b81b73b63ac3f5ac33881f
Blythe Waltman <blwa4890@colorado.edu>
2020-12-04 10:54:52 -0700

    Comments added for Rook Class

--- classes/Rook.py
+++ classes/Rook.py
@@ -15,10 +15,11 @@ class Rook(Piece):
     def giveMoveList(self, board):
         moveList = []
 
-        row = self.position[0] + 1
-        column = self.position[1]
+        row = self.position[0] + 1 # one row above the rook's current spot
+        column = self.position[1] # The rook's current column
         blocked = False
 
+        # Check all spaces that are in the same column and above the rook until it is blocked by another piece
         while(row <= 7 and blocked == False):
             if(board.getPiece(row, column) == None):
                 moveList.append([row, column])
@@ -26,10 +27,11 @@ class Rook(Piece):
                 blocked = True
             row = row + 1
 
-        row = self.position[0] - 1
-        column = self.position[1]
+        row = self.position[0] - 1 # one row below the rook's current spot
+        column = self.position[1] # The rook's current column
         blocked = False
 
+        # Check all spaces that are in the same column and below the rook until it is blocked by another piece
         while(row >= 0 and blocked == False):
             if (board.getPiece(row, column) == None):
                 moveList.append([row, column])
@@ -37,10 +39,11 @@ class Rook(Piece):
                 blocked = True
             row = row - 1
 
-        row = self.position[0]
-        column = self.position[1] + 1
+        row = self.position[0] # The rooks current row
+        column = self.position[1] + 1 # One space to the right of the rook
         blocked = False
 
+        # Check all spaces that are in the same row and to the right of the rook until it is blocked by another piece
         while (column <= 7 and blocked == False):
             if (board.getPiece(row, column) == None):
                 moveList.append([row, column])
@@ -48,10 +51,11 @@ class Rook(Piece):
                 blocked = True
             column = column + 1
 
-        row = self.position[0]
-        column = self.position[1] - 1
+        row = self.position[0] # The rook's current row
+        column = self.position[1] - 1 # One space to the left of the rook
         blocked = False
 
+        # Check all spaces that are in the same row and to the left of the rook until it is blocked by another piece
         while (column >= 0 and blocked == False):
             if (board.getPiece(row, column) == None):
                 moveList.append([row, column])
@@ -65,10 +69,11 @@ class Rook(Piece):
     def giveCaptureList(self,board):
         captureList = []
 
-        row = self.position[0] + 1
-        column = self.position[1]
+        row = self.position[0] + 1 # One row above the rooks current spot
+        column = self.position[1] # The rooks current column
         blocked = False
 
+        # Check all spaces that are in the same column and above the rook until it is blocked by its own color piece or can capture an any piece
         while (row <= 7 and blocked == False):
             if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
                 captureList.append([row, column])
@@ -77,10 +82,11 @@ class Rook(Piece):
                 blocked = True
             row = row + 1
 
-        row = self.position[0] - 1
-        column = self.position[1]
+        row = self.position[0] - 1 # One row below the rook's current spot
+        column = self.position[1] # The rooks current column
         blocked = False
 
+        # Check all spaces that are in the same column and below the rook until it is blocked by its own color piece or can capture an any piece
         while (row >= 0 and blocked == False):
             if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
                 captureList.append([row, column])
@@ -89,10 +95,11 @@ class Rook(Piece):
                 blocked = True
             row = row - 1
 
-        row = self.position[0]
-        column = self.position[1] + 1
+        row = self.position[0] # The rooks current row
+        column = self.position[1] + 1 # One spot to the right of the Rook
         blocked = False
 
+        # Check all spaces that are in the same crow and to the right of the rook until it is blocked by its own color piece or can capture an enemy piece
         while (column <= 7 and blocked == False):
             if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
                 captureList.append([row, column])
@@ -101,10 +108,11 @@ class Rook(Piece):
                 blocked = True
             column = column + 1
 
-        row = self.position[0]
-        column = self.position[1] - 1
+        row = self.position[0] # The rooks current row
+        column = self.position[1] - 1 # One spot to the left of the Rook
         blocked = False
 
+        # Check all spaces that are in the same crow and to the left of the rook until it is blocked by its own color piece or can capture an enemy piece
         while (column >= 0 and blocked == False):
             if (board.getPiece(row, column) != None and board.getPiece(row, column).getColor() != self.color):
                 captureList.append([row, column])
